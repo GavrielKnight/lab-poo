b4f7334f7b5330c428c57a76028c19dbe6f96d70
Gavriel <253627515+GavrielKnight@users.noreply.github.com>
2026-05-23 03:01:05 -0600

    Agrega manual de usuario y manual tecnico

--- pia/submenus/src/submenus/MainLayout.java
+++ pia/submenus/src/submenus/MainLayout.java
@@ -6,5 +6,6 @@ public class MainLayout {
     @FXML private void clientesBotonPresionado(){ if (Main.indiceUsuario >= 0) Main.loadPage("menuClientes.fxml"); }
     @FXML private void membresiasBotonPresionado(){ if (Main.indiceUsuario >= 0) Main.loadPage("menuMembresias.fxml"); }
     @FXML private void inventarioBotonPresionado(){ if (Main.indiceUsuario >= 0) Main.loadPage("menuInventario.fxml"); }
+    @FXML private void calendarioBotonPresionado(){ if (Main.indiceUsuario >= 0) Main.loadPage("menuCalendario.fxml"); }
     @FXML private void usuarioBotonPresionado(){ if (Main.indiceUsuario >= 0) Main.loadPage("menuUsuario.fxml"); }
 }
